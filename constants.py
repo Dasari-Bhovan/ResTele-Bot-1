a2448c0cd53ad04960846c040bb5219e0a56eb57
GPraneeth22 <96370123+gpraneeth22@users.noreply.github.com>
2023-02-10 22:52:29 +0530

    Initial Commit

---
+++ constants.py
@@ -0,0 +1,87 @@
+import os
+import pymongo
+
+# DATABASE CONNECTIVITY
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db = client['Student_TeleBot']
+res = client[""]
+# register = db['Student_Registrations']
+print("Connected Successfully")
+
+years = [19,20,21,22]
+student_registrations = {}
+for y in years:
+    student_registrations[y] = db[f'R{y} Student Registrations']
+# print(student_registrations)
+
+# USER AUTHORIZATION FUNCTION
+def auth_user(message):
+    registered = None
+    for y in years:
+        if student_registrations[y].find_one({'_id':message.chat.id}) == registered:
+            continue
+        else:
+            registered = True
+            break
+    return registered
+
+semesters = ['1-1','1-2','2-1','2-2','3-1','3-2','4-1','4-2']
+
+commands = ['/start','/register','/results','/studentsummary','/departmentsummary', '/help']
+
+test_ids = ['20bq1a4213@vvit.net','20bq1a4216@vvit.net','20bq1a4222@vvit.net','20bq1a4264@vvit.net']
+
+# Messages or Replies
+start_msg = '''
+Enter Roll number to view all semester results. Like this, For example 20BQ1A4762
+Enter Roll number with semester to view particular semester results. Like this, For example 20BQ1A4762 2-2
+'''
+
+help_msg ='''
+/start - Start the conversation
+/register - To get Latest results ASAP
+/help
+
+If you're facing any problem, Please "CLEAR CHAT HISTORY" and retry.
+Still facing the problem?
+Reach us at:
+vvitresults.bot@gmail.com
+'''
+
+report = '\n⚠️Revaluation & Supply results will be updated soon!'
+
+wrong_msg = '''Please check your Roll Number❗️'''
+
+auth_msg = '''🔺️You're not registered yet🔺️ \nTo register Use  /register'''
+
+departments = {
+    '01':'CIV',
+    '02':'EEE',
+    '03':'MEC',
+    '04':'ECE',
+    '05':'CSE',
+    '12':'INF',
+    '42':'CSM',
+    '47':'CIC',
+    '49':'CSO',
+    '54':'AID',
+    '61':'AIM'
+}
+dept_names = list(departments.values())
+
+dept_codes = list(departments.keys())
+
+dept_names_msg = '''Departments:\n{'CIV','EEE','MEC','ECE','CSE','IT','CSM','CIC','CSO','AID','AIM'}'''
+
+dept_msg = '''Enter **BATCH<space>DEPARTMENT<space>SEM** to get semester and that semester's each subject analysis\ni.e *19 civ 2-1*\n
+Enter **batch<space>department** to get department's Semester Analysis\ni.e *19 civ*'''
+
+otp_dict = {}
+
+res_otp_dict = {}
+
+res_roll_dict = {}
+
+current_ids = []
+
+messages = ['hi', 'hello', 'hey']
