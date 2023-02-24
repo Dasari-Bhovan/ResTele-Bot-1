3c3405e3820cdd507ee7f4fa011bc3c977927a8f
GPraneeth22 <96370123+gpraneeth22@users.noreply.github.com>
2023-02-24 15:21:03 +0530

    DB UPDATE

--- database.py
+++ database.py
@@ -7,5 +7,5 @@ client = pymongo.MongoClient("mongodb+srv://20bq1a4222:0URpI17aE6qph4sJ@restele-
 # ------------- VARIABLES THAT ARE USED TO ACCESS THE DATABASE --------------
 # FOR STUDENT REGISTRATION 
 stu_registration = client['Student_Registrations']
-# FOR RESULTS #
+# FOR RESULTS
 results = client["Results"]
